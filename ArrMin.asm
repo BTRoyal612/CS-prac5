a95fb120e7de409c3449fbe80c96c406a9889022
gbhoang <gb.hoang02@gmail.com>
2022-04-30 02:13:09 +0930

    task3.1

--- ArrMin.asm
+++ ArrMin.asm
@@ -2,3 +2,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @R2
+    D=M
+    @END
+    D;JLE
+    @N
+    M=1
+    @R1
+    A=M
+    D=M
+    @R0
+    M=D
+(LOOP)
+    @N
+    D=M
+    @R2
+    D=D-M
+    @END
+    D;JEQ
+    @R1
+    D=M
+    @N
+    A=D+M
+    D=M
+    @R4
+    M=D
+    @R0
+    D=M-D
+    @N
+    M=M+1
+    @MIN
+    D;JGT
+
+(END)
+    @END
+    0;JMP
+
+(MIN)
+    @R4
+    D=M
+    @R0
+    M=D
+    @LOOP
+    0;JMP
